9420a08cd9c4cf53409c9fb8c782bb87d475ba7f
Shashwat Dwivedi <121190747+shashwatdwi176@users.noreply.github.com>
2023-05-27 17:15:35 +0530

    Add files via upload
    added dependencies

--- food_rep.c
+++ food_rep.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_RECIPES 10
 
@@ -41,81 +42,120 @@ void addDependency(RecipeGraph* graph, int recipeIndex, int dependencyIndex) {
     }
 }
 
-void generateRecipesDFS(const RecipeGraph* graph, int currentRecipe, bool* visited) {
-    visited[currentRecipe] = true;
-
-    printf("Recipe: %s\n", graph->recipes[currentRecipe].name);
+void generateRecipeHelper(const RecipeGraph* graph, int recipeIndex, bool* visited) {
+    visited[recipeIndex] = true;
+    printf("Recipe: %s\n", graph->recipes[recipeIndex].name);
     printf("Steps:\n");
-    for (int i = 0; i < graph->recipes[currentRecipe].numSteps; i++) {
-        printf("%d. %s\n", i + 1, graph->recipes[currentRecipe].steps[i]);
+    for (int i = 0; i < graph->recipes[recipeIndex].numSteps; i++) {
+        printf("%d. %s\n", i + 1, graph->recipes[recipeIndex].steps[i]);
     }
     printf("\n");
 
     for (int i = 0; i < graph->numVertices; i++) {
-        if (graph->adjacencyMatrix[currentRecipe][i] && !visited[i]) {
-            generateRecipesDFS(graph, i, visited);
+        if (graph->adjacencyMatrix[recipeIndex][i] && !visited[i]) {
+            generateRecipeHelper(graph, i, visited);
         }
     }
 }
 
-void generateRecipes(const RecipeGraph* graph) {
-    bool visited[MAX_RECIPES];
+void generateRecipe(const RecipeGraph* graph, const char* recipeName) {
+    int recipeIndex = -1;
     for (int i = 0; i < graph->numVertices; i++) {
-        visited[i] = false;
-    }
-
-    printf("Generated Recipes:\n");
-    for (int i = 0; i < graph->numVertices; i++) {
-        if (!visited[i]) {
-            generateRecipesDFS(graph, i, visited);
+        if (strcmp(graph->recipes[i].name, recipeName) == 0) {
+            recipeIndex = i;
+            break;
         }
     }
+
+    if (recipeIndex == -1) {
+        printf("Recipe '%s' not found.\n", recipeName);
+        return;
+    }
+
+    bool visited[MAX_RECIPES] = { false };
+    generateRecipeHelper(graph, recipeIndex, visited);
+}
+
+void printMenu() {
+    printf("\nMenu:\n");
+    printf("1. Add Recipe\n");
+    printf("2. Add Dependency\n");
+    printf("3. Generate Recipe\n");
+    printf("4. Exit\n");
 }
 
 int main() {
     RecipeGraph graph = createRecipeGraph();
+    int choice;
 
-    Recipe pizzaRecipe = {
-        .name = "Pizza",
-        .steps = {
-            "Prepare the dough",
-            "Add sauce and toppings",
-            "Bake in the oven",
-            "Slice and serve"
-        },
-        .numSteps = 4
-    };
+    do {
+        printMenu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    Recipe pastaRecipe = {
-        .name = "Pasta",
-        .steps = {
-            "Boil water",
-            "Add pasta and cook",
-            "Drain the pasta",
-            "Add sauce and serve"
-        },
-        .numSteps = 4
-    };
+        switch (choice) {
+            case 1: {
+                if (graph.numVertices >= MAX_RECIPES) {
+                    printf("Maximum number of recipes reached.\n");
+                } else {
+                    Recipe newRecipe;
+                    printf("Enter recipe name: ");
+                    scanf(" %[^\n]", newRecipe.name);
+                    printf("Enter number of steps: ");
+                    scanf("%d", &newRecipe.numSteps);
+                    printf("Enter the steps:\n");
+                    for (int i = 0; i < newRecipe.numSteps; i++) {
+                        printf("Step %d: ", i + 1);
+                        scanf(" %[^\n]", newRecipe.steps[i]);
+                    }
+                    addRecipe(&graph, &newRecipe);
+                    printf("Recipe added successfully.\n");
+                }
+                break;
+            }
 
-    Recipe saladRecipe = {
-        .name = "Salad",
-        .steps = {
-            "Wash and chop the vegetables",
-            "Mix the ingredients",
-            "Add dressing",
-            "Serve"
-        },
-        .numSteps = 4
-    };
+            case 2: {
+                if (graph.numVertices < 2) {
+                    printf("Not enough recipes to add dependencies.\n");
+                } else {
+                    int recipeIndex, dependencyIndex;
+                    printf("Enter recipe index: ");
+                    scanf("%d", &recipeIndex);
+                    printf("Enter dependency index: ");
+                    scanf("%d", &dependencyIndex);
 
-    addRecipe(&graph, &pizzaRecipe);
-    addRecipe(&graph, &pastaRecipe);
-    addRecipe(&graph, &saladRecipe);
+                    if (recipeIndex < 0 || recipeIndex >= graph.numVertices ||
+                        dependencyIndex < 0 || dependencyIndex >= graph.numVertices) {
+                        printf("Invalid recipe or dependency index.\n");
+                    } else {
+                        addDependency(&graph, recipeIndex, dependencyIndex);
+                        printf("Dependency added successfully.\n");
+                    }
+                }
+                break;
+            }
 
-    addDependency(&graph, 0, 1); // Pizza depends on Pasta
-    addDependency(&graph, 0, 2); // Pizza depends on Salad
+            case 3: {
+                if (graph.numVertices < 1) {
+                    printf("No recipes added.\n");
+                } else {
+                    char recipeName[50];
+                    printf("Enter recipe name: ");
+                    scanf(" %[^\n]", recipeName);
+                    generateRecipe(&graph, recipeName);
+                }
+                break;
+            }
 
-    generateRecipes(&graph);
+            case 4:
+                printf("Exiting program.\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    } while (choice != 4);
 
     return 0;
 }
